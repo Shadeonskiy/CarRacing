7f63ad707d6b5ed52cdec6be707c50b80b2177e7
Shadeonskiy <shadeonskiy@gmail.com>
2023-02-05 20:00:56 +0200

    Added test files

---
+++ tests/TestSpritesheetLoader.py
@@ -0,0 +1,9 @@
+import unittest
+from spriteloader import SpriteSheet
+
+class TestSpriteLoader(unittest.TestCase):
+    def setUp(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/TestUtils.py
@@ -0,0 +1,9 @@
+import unittest
+from utils import blit_rotate_center, scale_image, get_scale_ratio
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
