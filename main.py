cec5008dc8499ae2ff79e7a3ad4052dcd33e6239
Shadeonskiy <shadeonskiy@gmail.com>
2023-02-05 20:00:04 +0200

    Added argparse
    Argparse for some car characteristics

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import constants
 from utils import scale_image
 from spriteloader import SpriteSheet
 from render import ObjectRenderer
+import argparse
 
 WIN = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
 pygame.display.set_caption("Car Racing Game")
@@ -10,7 +11,21 @@ pygame.display.set_caption("Car Racing Game")
 run = True
 clock = pygame.time.Clock()
 
+def init_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mv', '--max_velocity', default=4, type=int, choices=range(1, 11), help="set max velocity for player car")
+    parser.add_argument('--rv', '--rotation_velocity', default=4, type=int, choices=range(1, 6), help="set rotation velocity for player car")
+    parser.add_argument('--cc', '--car_color', default="red", choices=["blue","red","green","yellow","purple"], help="choose player car color")
+    args = parser.parse_args(['--mv', '4', '--rv', '2','--cc', 'yellow'])
+    return args
+
 if __name__ == "__main__":
+    args = init_argparser()
+    print(f'Player Car Characteristics\n'
+          f'max velocity: {args.mv} px/s\n'
+          f'rotation velocity: {args.rv} px/s\n'
+          f'color: {args.cc}\n')
+
     cars_spritesheet = SpriteSheet("Cars", "Sprites")
     cars_spritesheet.get_sprites()
     objects = ObjectRenderer()
